9a711ce861e0b415a0fd92699d42e5aded90eed5
i2key <itsuki.k@gmail.com>
2012-04-05 03:52:34 +0900

    Scalaの勉強用に書いたコード類をいれていきます。

---
+++ ScalaStudy/src/ReFollowBot.scala
@@ -0,0 +1,27 @@
+import twitter4j.Twitter
+import twitter4j.TwitterFactory
+import twitter4j.TwitterException
+import twitter4j.Status
+import twitter4j.auth.AccessToken
+import twitter4j.IDs
+
+class ReFollowBot {
+
+  def main(args: Array[String]): Unit = {
+
+    val consumerKey = "CONSUMER_KEY";
+    val consumerSecret = "CONSUMER_SECRET";
+    val accessToken = "ACCESS_TOKEN";
+    val accessTokenSecret = "ACCESS_TOKEN_SECRET";
+
+    val twitter: Twitter = new TwitterFactory().getInstance();
+    twitter.setOAuthConsumer(consumerKey, consumerSecret);
+    twitter.setOAuthAccessToken(new AccessToken(accessToken, accessTokenSecret));
+
+    val friends = twitter.getFriendsIDs(twitter.getId(), -1).getIDs();
+    val followers = twitter.getFollowersIDs(twitter.getId(), -1).getIDs();
+
+    followers.filter(!friends.contains(_)).foreach(twitter.createFriendship(_));
+
+  }
+}
